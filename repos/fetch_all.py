cbbab9e0f3d97d283c7e59909e9b7e113616cab8
Kirill Smirenko <k.smirenko@gmail.com>
2017-10-22 22:09:56 +0300

    Add repo fetching script

---
+++ repos/fetch_all.py
@@ -0,0 +1,40 @@
+import subprocess
+
+repos = [
+    "git@github.com:Raizlabs/DBFlow.git",
+    "https://github.com/dbacinski/Design-Patterns-In-Kotlin.git",
+    "git@github.com:kittinunf/Fuel.git",
+    "git@github.com:SalomonBrys/Kodein.git",
+    "https://github.com/vicboma1/Kotlin-Koans.git",
+    "https://github.com/dodyg/Kotlin101.git",
+    "git@github.com:SalomonBrys/Kotson.git",
+    "git@github.com:ReactiveX/RxKotlin.git",
+    "git@github.com:http4k/http4k.git",
+    "git@github.com:Kotlin/kotlin-coroutines.git",
+    "https://github.com/JetBrains/kotlin-examples.git",
+    "https://github.com/Kotlin/kotlin-koans.git",
+    "git@github.com:JetBrains/kotlin.git",
+    "git@github.com:Kotlin/kotlinx.coroutines.git",
+    "git@github.com:kohesive/kovert.git",
+    "git@github.com:ktorio/ktor.git",
+    "git@github.com:nhaarman/mockito-kotlin.git",
+    "git@github.com:perwendel/spark-kotlin.git",
+    "git@github.com:wasabifx/wasabi.git",
+
+    "git@github.com:arturbosch/detekt.git",
+    "git@github.com:edvin/tornadofx.git",
+    "git@github.com:jankotek/mapdb.git",
+    "git@github.com:requery/requery.git",
+    "git@github.com:square/sqldelight.git",
+    "git@github.com:JetBrains/Exposed.git",
+]
+
+
+def git(*args):
+    return subprocess.check_call(['git'] + list(args))
+
+
+subprocess.run("ls")
+git("status")
+for repo in repos:
+    git("clone", repo)
