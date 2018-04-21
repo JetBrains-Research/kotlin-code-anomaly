6b7181a1c8c41d96ec52eca192d86bd44d7c5116
Kirill Smirenko <k.smirenko@gmail.com>
2018-04-21 18:32:28 +0300

    Edit build scripts, adding visualizer-gui

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ mv feature-calc/build/libs/features.jar features.jar || exit 1
 echo "Created or replaced features.jar"
 
 ./gradlew :visualizer:jar || exit 1
-mv visualizer/build/libs/visualizer.jar visualizer.jar || exit 1
-echo "Created or replaced visualizer.jar"
+mv visualizer/build/libs/visualizer-cli.jar visualizer-cli.jar || exit 1
+echo "Created or replaced visualizer-cli.jar"
+
+./gradlew :visualizer-gui:jar || exit 1
+mv visualizer-gui/build/libs/visualizer-gui.jar visualizer-gui.jar || exit 1
+echo "Created or replaced visualizer-gui.jar"
 
 echo "Done."
 echo "To launch metric extraction, run: ./calc-features"
-echo "To launch interactive anomaly visualizer, run: java -jar visualizer.jar"
